d5e0036251b31bdea6a8822c50fc6a168353c908
songmingu(win) <min9nim@gmail.com>
2018-07-14 02:12:31 +0900

    build

--- src/pages/Local.js
+++ src/pages/Local.js
@@ -29,13 +29,13 @@ export default class Local extends React.Component {
         level: 4 //지도의 레벨(확대, 축소 정도)
     };
     
-    
+
     this.map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴      
   }
 
 
   componentDidUpdate(){
-    var geocoder = new daum.maps.services.Geocoder();
+    const geocoder = new daum.maps.services.Geocoder();
     geocoder.addressSearch(this.state.word, (result, status) => {
         if (status === daum.maps.services.Status.OK) {
             this.map.setCenter(new daum.maps.LatLng(result[0].y, result[0].x));
