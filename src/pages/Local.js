9fb54186d4f1000271dd20bb1bd461729e7af44e
songmingu(mac) <min9nim@gmail.com>
2018-07-24 14:48:50 +0900

    현위치 표시

--- src/pages/Local.js
+++ src/pages/Local.js
@@ -22,15 +22,17 @@ export default class Local extends React.Component {
   }
 
   componentDidMount(){
-    console.log("## componentDidMount")
-    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new daum.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-        level: 4 //지도의 레벨(확대, 축소 정도)
-    };
-    
-
-    this.map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴      
+    console.log("## componentDidMount");
+    navigator.geolocation.getCurrentPosition(position => {
+        var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        var options = { //지도를 생성할 때 필요한 기본 옵션
+            center: new daum.maps.LatLng(position.coords.latitude, position.coords.longitude), //지도의 중심좌표.
+            level: 4 //지도의 레벨(확대, 축소 정도)
+        };
+        this.map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴      
+    }, err => {
+        alert(err.message)
+    })
   }
 
 
